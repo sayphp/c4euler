bd29e45190b1ad2b10a923933dc6c2251bcb4e70
刘潇 <liuxiao@sanhao.com>
2015-12-29 18:35:11 +0800

    做完第十题，注意int和long int

--- 10.c
+++ 10.c
@@ -3,14 +3,14 @@
 //求200w以内质数的和
 
 int main(void){
-	int x = 1, max = 2000000, rs=0;
+	long int x = 1, max = 2000000, rs=0;
     while(x < max){	
 		if(x==1) goto jump;
 		if(x<4)	goto sum;
 		if(x%2==0) goto jump;//因为我的实现自动跳过了偶数，所以这条不存在，这么看貌似比最优有所优化-。-
 		if(x<9) goto sum;
 		if(x%3==0)	goto jump;
-		int r = floor(sqrt(x)),
+		long int r = floor(sqrt(x)),
 			y = 5;
 		while(y<=r){
 			if(x%y==0) goto jump;
@@ -19,8 +19,9 @@ int main(void){
 		}
 
 		sum:rs += x;
+		//printf("The rs is %d\r\n", rs);
 		jump:;
-		x++;
+		x += 2;
 	}
-	printf("The sum is %d\r\n", rs);
+	printf("The sum is %ld\r\n", rs+2);//循环的时候略过所有偶数，然后补足2
 }
